726907eb19263baa8c20ea3ef4d47c584b1b20de
devfoFikiCar <fiki.obradovic@gmail.com>
2020-12-16 18:05:34 +0100

    Display all lectures for student

---
+++ NDP/app/src/main/java/com/devoFikiCar/ndp/ui/lecture/LectureItem.java
@@ -0,0 +1,17 @@
+package com.devoFikiCar.ndp.ui.lecture;
+
+public class LectureItem {
+    private String lectureTitle;
+
+    public LectureItem(String lectureTitle) {
+        this.lectureTitle = lectureTitle;
+    }
+
+    public String getLectureTitle() {
+        return lectureTitle;
+    }
+
+    public void setLectureTitle(String lectureTitle) {
+        this.lectureTitle = lectureTitle;
+    }
+}
