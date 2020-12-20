fcb94e0792f8898579474037821d18f655f59c94
devfoFikiCar <fiki.obradovic@gmail.com>
2020-12-20 18:06:57 +0100

    copyright

--- NDP/app/src/main/java/com/devoFikiCar/ndp/util/Lecture.java
+++ NDP/app/src/main/java/com/devoFikiCar/ndp/util/Lecture.java
@@ -3,7 +3,6 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Filip Obradović <fiki.obradovic@gmail.com> 2020
- *
  */
 
 package com.devoFikiCar.ndp.util;
