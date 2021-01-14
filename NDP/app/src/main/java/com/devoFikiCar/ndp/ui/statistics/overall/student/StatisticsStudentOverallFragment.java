ddf2e87cd4dc65f525f35fa5cc9149b58c51209f
devfoFikiCar <fiki.obradovic@gmail.com>
2021-01-14 23:54:40 +0100

    StatisticsOverAll

---
+++ NDP/app/src/main/java/com/devoFikiCar/ndp/ui/statistics/overall/student/StatisticsStudentOverallFragment.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) devfoFikiCar - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Filip Obradović <fiki.obradovic@gmail.com> 2021
+ */
+
+package com.devoFikiCar.ndp.ui.statistics.overall.student;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.devoFikiCar.ndp.R;
+
+public class StatisticsStudentOverallFragment extends Fragment {
+
+    private StatisticsStudentOverallViewModel mViewModel;
+
+    public static StatisticsStudentOverallFragment newInstance() {
+        return new StatisticsStudentOverallFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.statistics_student_overall_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(StatisticsStudentOverallViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
