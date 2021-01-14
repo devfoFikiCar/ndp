fb043eb48325e1e30a18e43553a7847de08488f5
devfoFikiCar <fiki.obradovic@gmail.com>
2021-01-15 00:47:19 +0100

    Copyright update

--- NDP/app/src/main/java/com/devoFikiCar/ndp/util/User.java
+++ NDP/app/src/main/java/com/devoFikiCar/ndp/util/User.java
@@ -2,7 +2,7 @@
  * Copyright (C) devfoFikiCar - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Filip Obradović <fiki.obradovic@gmail.com> 2020
+ * Written by Filip Obradović <fiki.obradovic@gmail.com> [2020-2021]
  */
 
 package com.devoFikiCar.ndp.util;
